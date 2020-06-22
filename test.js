080204325259f69727029fbc1d7b5b8e68418286
Jay Zawrotny <jayzawrotny@gmail.com>
2020-06-22 18:21:53 -0400

    WIP

--- test.js
+++ test.js
@@ -1,20 +1,41 @@
 const _ = require('highland');
 const fs = require('fs');
-const Pump = require('util').promisify(require('stream').pipeline);
+const stream = require("stream");
+const Pump = require('util').promisify(stream.pipeline);
 
 const double = x => x*2;
 
-async function main () {
-    await fs.createReadStream('test.info', { encoding: 'utf8' })
-        .pipe(_())
-        .split()
-        .map(double)
-        .tap(x => console.log('Doubled = ', x))
-        .collect()
-        .toPromise(Promise)
+const consumer = _.curry(({ func, msg }, data) => _(data)
+    .split()
+    .map(func)
+    .tap(x => console.log(msg, x))
+);
+
+function transformStream (xfn, opts) {
+    const source = new stream.Transform(Object.assign({
+        transform (chunk, encoding, callback) {
+            this.push(chunk);
+            callback();
+        },
+
+        flush (callback) {
+            console.log("Flushing");
+            callback();
+        }
+    }, opts))
+
+    return transform;
+}
+
+const main = async () => {
+    const fileStream = fs.createReadStream('test.info', { encoding:  'utf8' });
+    const publisher = consumer({ func: double, msg: 'Doubled = ' });
+
+    await Pump(fileStream, transformStream(publisher));
 
     console.log('DONE');
-}
+};
+
 
 main()
     .catch(err => {
