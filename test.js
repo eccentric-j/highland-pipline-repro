0109cb903e645bec86c9ead2198e05123592b026
Jay Zawrotny <jayzawrotny@gmail.com>
2020-06-22 17:23:15 -0400

    Highland centric solution

--- test.js
+++ test.js
@@ -4,21 +4,17 @@ const Pump = require('util').promisify(require('stream').pipeline);
 
 const double = x => x*2;
 
-const consumer = _.curry(({ func, msg }, data) => _(data)
-    .split()
-    .map(func)
-    .tap(x => console.log(msg, x))
-    .toNodeStream()
-);
-
-const main = async () => {
-    const fileStream = fs.createReadStream('test.info', { encoding:  'utf8' });
-    const publisher = consumer({ func: double, msg: 'Doubled = ' });
-
-    await Pump(fileStream, publisher);
+async function main () {
+    await fs.createReadStream('test.info', { encoding: 'utf8' })
+        .pipe(_())
+        .split()
+        .map(double)
+        .tap(x => console.log('Doubled = ', x))
+        .collect()
+        .toPromise(Promise)
 
     console.log('DONE');
-};
+}
 
 main()
     .catch(err => {
